d6c2b8d9d9a29b5b70cdc572e3f4750c6856d1ad
mland_laptop <mland@DESKTOP-PNEL2RP>
2016-10-06 16:16:18 -0400

    this snapshot is a very basic react app with some basic state change operations, good for trying out browser dev tools

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "Mike",
+    }
+  }
   render() {
+    setTimeout(() => {
+      this.setState({name: "Chris"})
+    }, 1000)
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+      {this.state.name}
+        <Header />
+        <Footer />
       </div>
     );
   }
